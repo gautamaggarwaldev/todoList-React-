0bec61cdb2d6468f24d321c6f051528bbae2843b
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2024-10-20 15:29:35 +0530

    edit todo and save

--- src/Component/Todo/Todo.jsx
+++ src/Component/Todo/Todo.jsx
@@ -1,17 +1,24 @@
 import { useState } from "react";
 
-function Todo({todoData, isFinished, id, changeFinished, onDelete}) {
+function Todo({ todoData, isFinished, id, changeFinished, onDelete, onEdit }) {
 
     const [finished, setFinished] = useState(isFinished);
-
+    const [isEdit, setIsEdit] = useState(false);
+    const [editText, setEditText] = useState(todoData);
     return (
         <div>
         <input type="checkbox" checked={finished} onChange={(e)=> {
             setFinished(e.target.checked);
             changeFinished(e.target.checked);
             }}/>
-        {todoData}
-        <button>Edit</button>
+
+        {isEdit ?  <input type='text' value={editText} onChange={e => setEditText(e.target.value)} /> : <span>{todoData}</span>}
+
+        <button onClick={()=> {
+            setIsEdit(!isEdit);
+            onEdit(editText)
+        }}>{!isEdit ? 'Edit' : 'Save'}</button>
+
         <button onClick={onDelete}>Delete</button>
 
         </div>
--- src/Component/TodoList/TodoList.jsx
+++ src/Component/TodoList/TodoList.jsx
@@ -26,6 +26,15 @@ function TodoList() {
                                                     const updatedList = list.filter(t => t.id != todo.id);
                                                     setList(updatedList);
                                                 }}
+                                                onEdit={(todoText)=> {
+                                                    const updatedList = list.map(t => {
+                                                        if(t.id == todo.id) {
+                                                            todo.todoData = todoText;
+                                                        }
+                                                        return t;
+                                                    });
+                                                    setList(updatedList);
+                                                }}
                                             />)}
 
         </div>
